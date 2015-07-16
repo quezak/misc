65db0146f2636fa9084cb57539c82c6459ddafd5
Artur Kozak <kozak@google.com>
2015-07-16 13:23:27 -0700

    fix toggle_monitor for maximized windows

--- bin/toggle_monitor.sh
+++ bin/toggle_monitor.sh
@@ -5,8 +5,8 @@
 # Check these with xwininfo on a maximized window.
 lwidth=1920
 lheight=1008
-rwidth=1680
-rheight=1028
+rwidth=1920
+rheight=1200
 
 # Get current size and position.
 window=`xdotool getactivewindow`
@@ -15,20 +15,37 @@ curw=`xwininfo -id $window | grep "Width" | awk '{print $2}'`
 curh=`xwininfo -id $window | grep "Height" | awk '{print $2}'`
 
 # Don't change window size if it fits the other monitor, shrink it otherwise.
-# Maximized (even in only one dimension) windows seem to automatically get re-maximized properly.
 neww=-1
 newh=-1
 
+# Maximized windows don't move properly under some mysterious circumstances, so we have
+# to unmaximize them while moving.
+# FIXME: this works just with fully maximized windows, will have to detect
+#        horizontal and vertical maximization separately
+maximized=false
+
 # Check on which monitor the window is and set new values.
 if [ "$curx" -lt "$lwidth" ]; then
+    if [ $curw -eq $lwidth ]; then maximized=true; fi
     newx=$[curx+lwidth]
     if [ $curw -gt $rwidth ]; then neww=$rwidth; fi
     if [ $curh -gt $rheight ]; then newh=$rheight; fi
 else
+    if [ $curw -eq $rwidth ]; then maximized=true; fi
     newx=$[curx-lwidth]
     if [ $curw -gt $lwidth ]; then neww=$lwidth; fi
     if [ $curh -gt $lheight ]; then newh=$lheight; fi
 fi
 
+# Temporarily unmaximize if needed
+if $maximized; then
+    wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
+fi
+
 # Move the window.
 wmctrl -r :ACTIVE: -e 0,$newx,-1,$neww,$newh
+
+# Maximize back if needed
+if $maximized; then
+    wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
+fi
